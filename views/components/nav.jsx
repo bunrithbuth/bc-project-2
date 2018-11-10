b18c7340474cea7a41d257eebab710d797fa4877
benohhh <b3njaminoh@gmail.com>
2018-11-10 11:28:19 -0800

    fixed header and footer

--- views/components/nav.jsx
+++ views/components/nav.jsx
@@ -3,15 +3,13 @@ const Fragment = React.Fragment
 
 const Nav = props =>
 <Fragment>
-  <div className="top-bar" id="example-menu">
+  <div className="top-bar" id="nav">
     <div className="top-bar-left">
-      <ul className="dropdown menu" data-dropdown-menu>
-        <li className="menu-text">Site Title</li>
-        <li>
-          <a href="#">One</a>
-        </li>
-        <li><a href="#">Two</a></li>
-        <li><a href="#">Three</a></li>
+      <ul className="dropdown menu">
+        <li className="menu-text">Deez Polls</li>
+        <li><a href="#">Create a Poll</a></li>
+        <li><a href="#">View Polls</a></li>
+        <li><a href="#">About Us</a></li>
       </ul>
     </div>
   </div>
