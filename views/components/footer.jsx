4aa5270711d0646439397c8b85fae5670bac72bf
benohhh <b3njaminoh@gmail.com>
2018-11-10 13:43:31 -0800

    added vertical alignment to footer

--- views/components/footer.jsx
+++ views/components/footer.jsx
@@ -3,10 +3,9 @@ const Fragment = React.Fragment
 
 const Footer = props =>
   <Fragment>
-    <div className="grid-container fluid">
-      <div className="grid-x">
-        <div className="cell text-center"><h1>This be the footer</h1></div>
-      </div>
+    <div className="row align-bottom large-12" id="footer">
+      <div className="column text-center"><h1>Footer</h1></div>
+      <div className="column text-center"><h2>More footer</h2></div>
     </div>
   </Fragment>
 
