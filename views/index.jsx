071600f73225c03c122f58005ead14425950243b
benohhh <b3njaminoh@gmail.com>
2018-11-10 09:53:26 -0800

    added necessary cdn's

--- views/index.jsx
+++ views/index.jsx
@@ -2,17 +2,19 @@ const React = require('react')
 const Footer = require('./components/footer')
 
 const Page = () =>
-    <html lang="en">
+    <html lang='en'>
         <head>
             <meta charSet='UTF-8' />
             <meta name='viewport' content='width=device-width, initial-scale=1.0' />
             <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+            <link src='https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/css/foundation.css' />
             <title>Document</title>
         </head>
         <body>
             <h1>HELLO WORLD</h1>
             <Footer />
-            <script src="/js/app.js"></script>
+            <script src='/js/app.js' />
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0/js/foundation.js' />
         </body>
     </html>
 
