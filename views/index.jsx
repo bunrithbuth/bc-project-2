12312d9bc2a95251c98c89ba6a873ea30dfaa13d
Mearat Hou <mearatjames@gmail.com>
2018-11-15 18:44:14 -0800

    commit

--- views/index.jsx
+++ views/index.jsx
@@ -14,7 +14,6 @@ const Page = () =>
             <Nav />
             <Content />
             <Footer />
-            <script src='/js/app.js' />
             <Script />
         </body>
     </html>
