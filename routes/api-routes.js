f4358f73200b7484963ae93fff5fef71d91fe595
Thomas Nguyen <tealeafnguyen@gmail.com>
2018-11-15 17:02:31 -0800

    added page to manage polls

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,18 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get('/api/polls', (req, res) => {
-        db.Todo.findAll({}).then(function(dbTodo) {
+        db.Todo.findAll({}).then(function (dbTodo) {
             // We have access to the todos as an argument inside of the callback function
             res.json(dbTodo);
-        });res.render('index')
+        }); res.render('index')
+    })
+
+    app.get('/api/myPolls', (req, res) => {
+        console.log('ping')
+        db.polls.findAll({}).then(function (dbTodo) {
+            res.json(dbTodo);
+        });
     })
 
     app.get('/api/migrate', (req, res) => {
@@ -16,29 +23,29 @@ module.exports = function(app) {
         db.users.create({
             name: 'bunrith',
         })
-        .then( _users => {
-            db.polls.create({
-                type: 'star',
-                user_name: (_users).name,
-                is_private: 0
-            }).then( _polls => {
-                db.poll_entry.create({
-                    poll_id: (_polls).id,
-                    name: 'McDonalds',
-                    description: 'Im Lovin It',
-                    star_rating: 3.5,
-                    star_rating_count: 1,
-                    votes: null
-                }).then( _pollEntry => {
-                    db.user_votes.create({
-                        user_name: 'mearat',
-                        poll_entry_id: (_pollEntry).id,
+            .then(_users => {
+                db.polls.create({
+                    type: 'star',
+                    user_name: (_users).name,
+                    is_private: 0
+                }).then(_polls => {
+                    db.poll_entry.create({
+                        poll_id: (_polls).id,
+                        name: 'McDonalds',
+                        description: 'Im Lovin It',
                         star_rating: 3.5,
-                        vote: null
-                    })    
+                        star_rating_count: 1,
+                        votes: null
+                    }).then(_pollEntry => {
+                        db.user_votes.create({
+                            user_name: 'mearat',
+                            poll_entry_id: (_pollEntry).id,
+                            star_rating: 3.5,
+                            vote: null
+                        })
+                    })
                 })
             })
-        })
 
         res.render('index')
     })
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,12 @@
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get('/', (req, res) => {
         res.render('index')
     })
+
+
+    app.get("/manage", function (req, res) {
+        res.render('manager');
+    });
 };
