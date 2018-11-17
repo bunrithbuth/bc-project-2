cefcb0ed11307e9c66025be534fcbbfd9d199585
benohhh <b3njaminoh@gmail.com>
2018-11-17 10:05:33 -0800

    added polllisting.jsx

--- views/components/content.jsx
+++ views/components/content.jsx
@@ -5,8 +5,8 @@ const Content = props =>
   <Fragment>
     <div className="grid-container fluid content">
       <div className="grid-x">
-        <div className="row large-12 text-center"><h1 id="title">Welcome to Deez Polls</h1></div>
-          <p className="row large-12 text-center">The purpose of this app is to allow users to create polls to share publicly or privately. Guest users will able to participate in public polls but will have to register to participate in private polls.</p>
+        <div className="cell text-center"><h1 id="title">Welcome to Deez Polls</h1></div>
+          <p className="cell text-center">The purpose of this app is to allow users to create polls to share publicly or privately. Guest users will able to participate in public polls but will have to register to participate in private polls.</p>
       </div>
     </div>
   </Fragment>
--- views/components/footer.jsx
+++ views/components/footer.jsx
@@ -6,10 +6,8 @@ const Footer = props =>
     <div className="grid-container fluid footer">
       <div className="grid-x">
         <div className="cell text-center"><h1>This be the footer</h1></div>
-        <div className="row large-12">
-          <div className="column text-center"><a href="https://github.com/bunrithbuth/bc-project-2">Github</a></div>
-          <div className="column text-center">© Shookboyz 2018</div>
-        </div>
+          <div className="cell text-center"><a href="https://github.com/bunrithbuth/bc-project-2">Github</a></div>
+          <div className="cell text-center">© Shookboyz 2018</div>
       </div>
     </div>
   </Fragment>
