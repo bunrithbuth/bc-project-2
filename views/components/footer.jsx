0c572e2dbc5ba0acd04bcd8c446de4116012457f
benohhh <b3njaminoh@gmail.com>
2018-11-19 22:43:23 -0800

    fixed footer

--- views/components/footer.jsx
+++ views/components/footer.jsx
@@ -5,11 +5,14 @@ const Footer = props =>
   <Fragment>
     <div className="grid-container fluid footer">
       <div className="grid-x">
-        <div className="cell text-center"><h1>This be the footer</h1></div>
-          <div className="cell text-center"><a href="https://github.com/bunrithbuth/bc-project-2">Github</a></div>
-          <div className="cell text-center">© Shookboyz 2018</div>
+        <div className="cell large-auto"><strong>Resources</strong><br /> 
+          <p><a href="https://github.com/bunrithbuth/bc-project-2">Github</a></p> 
+        </div>
+        <div className="cell large-auto"><strong>Presented By:</strong><br />
+          <p>© Shookboyz 2018</p>
+        </div>
+        </div>
       </div>
-    </div>
   </Fragment>
 
 module.exports = Footer
