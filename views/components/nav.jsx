cf52c733afe6f301fcbbd7e656322c630777e07c
benohhh <b3njaminoh@gmail.com>
2018-11-24 13:55:53 -0800

    changed nav

--- views/components/nav.jsx
+++ views/components/nav.jsx
@@ -16,8 +16,8 @@ const Nav = props =>
           <li className="menu-text" id="deez"><a href="/">Deez Polls</a></li>
           <li><a href="/createpolls">Create a Poll</a></li>
           <li><a href="/mypolls">My Polls</a></li>
-          <li><a href="/aboutus">About Us</a></li>
           <li><a href="/polls">Community Polls</a></li>
+          <li><a href="/aboutus">About Us</a></li>
             <div className="top-bar-right">
             <ul className="menu">
                 <li><a className="top-bar-right" id="nav3" href="/signin">Log In</a></li>
