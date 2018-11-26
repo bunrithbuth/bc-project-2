259ce5adc96d26cffab6944b0998fdcf9c473032
BBUTH <BBUTH_EMAIL>
2018-11-26 15:29:50 -0800

    heroku db test 6

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ module.exports = {
     "development": {
       "username": process.env.CLEARDB_USERNAME,
       "password": process.env.CLEARDB_PASSWORD,
-      "database": CLEARDB_DB_NAME,
+      "database": process.env.CLEARDB_DB_NAME,
       "host": process.env.CLEARDB_HOST_URL,
       "dialect": "mysql"
     },
     "test": {
       "username": process.env.CLEARDB_USERNAME,
       "password": process.env.CLEARDB_PASSWORD,
-      "database": CLEARDB_DB_NAME,
+      "database": process.env.CLEARDB_DB_NAME,
       "host": process.env.CLEARDB_HOST_URL,
       "dialect": "mysql"
     },
     "production": {
       "username": process.env.CLEARDB_USERNAME,
       "password": process.env.CLEARDB_PASSWORD,
-      "database": CLEARDB_DB_NAME,
+      "database": process.env.CLEARDB_DB_NAME,
       "host": process.env.CLEARDB_HOST_URL,
       "dialect": "mysql"
     }
