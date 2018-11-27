67b876222e40f37a5de013eeecedef8265351a89
Thomas Nguyen <tealeafnguyen@gmail.com>
2018-11-27 12:20:21 -0800

    added account tab

--- public/js/global.js
+++ public/js/global.js
@@ -1,26 +1,25 @@
 $(document).foundation();
 
 let user = {
-    authUser () {
-        if(!localStorage.getItem('user')) {
-          return false
-        }else{
-          let user = JSON.parse(localStorage.getItem('user'))
-          $('.top-bar-right li').empty();
-          $('.top-bar-right li').append(`
+  authUser() {
+    if (!localStorage.getItem('user')) {
+      return false
+    } else {
+      $('.top-bar-right li').empty();
+      $('.top-bar-right li').append(`
             <a class="top-bar-right" id="nav3" onclick="user.logout()">Log Out</a>
           `);
-          $('.manager').css('display','block');
-        }
-      },
-      logout () {
-        localStorage.clear();
-        $('.top-bar-right li').empty();
-          $('.top-bar-right li').append(`
+      $('.manager').css('display', 'inline-block')
+    }
+  },
+  logout() {
+    localStorage.clear();
+    $('.top-bar-right li').empty();
+    $('.top-bar-right li').append(`
             <a class="top-bar-right" id="nav3" href="/signin">Log In</a>
           `);
-          $('.manager').css('display','none')
-      }
+    $('.manager').css('display', 'none')
+  }
 };
 
 user.authUser();
